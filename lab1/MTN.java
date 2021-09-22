7f6914ccaee91d5fd5ecd30c0b8d27c343267834
Mai Tien <tmai2218@gmail.com>
2021-09-22 13:24:36 +0300

    MTN

---
+++ lab1/MTN.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MTN {
+    public static void main(String[] args){
+
+        //Демонстрация хэш функции
+        String statement1 = "Welcome to the club Boddy!";
+        int hashvalue = statement1.hashCode();
+
+        System.out.println("Statement = " + statement1 + " Whose hash value = " + hashvalue);
+
+        //Розшифруємо масив
+        String[] list1 = {"Mai", "Tien"};
+        String[] list2 = {"Mai", "Tien"};
+
+        int hash1 = Arrays.hashCode(list1);
+        int hash2 = Arrays.hashCode(list2);
+
+        System.out.println("\nhash1 = " + hash1 + "\nhash2 = " + hash2);
+
+    }
+}
